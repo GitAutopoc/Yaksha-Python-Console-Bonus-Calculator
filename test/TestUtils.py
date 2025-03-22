9409a8956c9aa534d1877ad0737d7e51add0c5b9
GitAutopoc <98872904+GitAutopoc@users.noreply.github.com>
2025-03-22 14:53:13 +0530

    Update TestUtils.py

--- test/TestUtils.py
+++ test/TestUtils.py
@@ -2,6 +2,7 @@ from test.TestResults import TestResults
 from test.TestCaseResultDto import TestCaseResultDto
 import json
 import requests
+import os
 
 class TestUtils:
     GUID = "dc66f3c1-630f-40ab-8314-f7bb9ffcb71f"
@@ -22,9 +23,11 @@ class TestUtils:
 
         test_case_result_dto = TestCaseResultDto(test_name, test_type, 1, result_score, result_status, True, "")
         test_case_results[self.GUID] = test_case_result_dto
+        
+        hostName = os.environ.get('HOSTNAME')
+        attemptId = os.environ.get('ATTEMPT_ID')
 
-        test_results = TestResults(json.dumps(test_case_results), customData)
-
+        test_results = TestResults(json.dumps(test_case_results), customData, hostName, attemptId)
         final_result = json.dumps(test_results)
 
         requests.post(self.URL, final_result)
